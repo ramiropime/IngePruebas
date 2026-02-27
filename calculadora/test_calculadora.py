f491f346747ef49437fd44198852a17466340079
Ramiro Pimentel <ramiro.pimentel@uabc.edu.mx>
2026-02-26 19:05:35 -0800

    Se agrega prueba para subtract

--- calculadora/test_calculadora.py
+++ calculadora/test_calculadora.py
@@ -1,4 +1,9 @@
-from calculator import Calculadora
+from calculadora import Calculadora
+
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
+
+def test_subtract():
+    calc = Calculadora()
+    assert calc.subtract(5, 3) == 2
